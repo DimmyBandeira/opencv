62b4756f1a1ccce3a6306f63b5da725200815683
DRBANDEIRA <91505202+DimmyBandeira@users.noreply.github.com>
2023-02-16 18:41:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# facial_web
+Uma forma de colocar o reconhecimento facial no pc do cliente 
+
+https://github.com/opencv/opencv/releases
+
+https://docs.opencv.org/4.x/df/d6c/tutorial_js_face_detection_camera.html
+
---
+++ script.js
@@ -0,0 +1,215 @@
+
+
+let videoWidth, videoHeight;
+
+// se streaming de vídeo da câmera.
+let streaming = false;
+
+let video = document.getElementById('video');
+let canvasOutput = document.getElementById('canvasOutput');
+let canvasOutputCtx = canvasOutput.getContext('2d');
+let stream = null;
+
+
+
+
+function startCamera() {
+  if (streaming) return;
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(s) {
+    stream = s;
+    video.srcObject = s;
+    video.play();
+  })
+    .catch(function(err) {
+    console.log("An error occured! " + err);
+  });
+
+  video.addEventListener("canplay", function(ev){
+    if (!streaming) {
+      videoWidth = video.videoWidth;
+      videoHeight = video.videoHeight;
+      video.setAttribute("width", videoWidth);
+      video.setAttribute("height", videoHeight);
+      canvasOutput.width = videoWidth;
+      canvasOutput.height = videoHeight;
+      streaming = true;
+    }
+    startVideoProcessing();
+  }, false);
+}
+
+let faceClassifier = null;
+
+let src = null;
+let dstC1 = null;
+let dstC3 = null;
+let dstC4 = null;
+
+let canvasInput = null;
+let canvasInputCtx = null;
+
+let canvasBuffer = null;
+let canvasBufferCtx = null;
+
+function startVideoProcessing() {
+  if (!streaming) { console.warn("Inicie sua webcam"); return; }
+  canvasInput = document.createElement('canvas');
+  canvasInput.width = videoWidth;
+  canvasInput.height = videoHeight;
+  canvasInputCtx = canvasInput.getContext('2d');
+  
+  canvasBuffer = document.createElement('canvas');
+  canvasBuffer.width = videoWidth;
+  canvasBuffer.height = videoHeight;
+  canvasBufferCtx = canvasBuffer.getContext('2d');
+  
+  srcMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
+  grayMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC1);
+  
+  faceClassifier = new cv.CascadeClassifier();
+  faceClassifier.load('haarcascade_frontalface_default.xml');
+    
+  requestAnimationFrame(processVideo);
+}
+
+function processVideo() {
+  canvasInputCtx.drawImage(video, 0, 0, videoWidth, videoHeight);
+  let imageData = canvasInputCtx.getImageData(0, 0, videoWidth, videoHeight);
+  srcMat.data.set(imageData.data);
+  cv.cvtColor(srcMat, grayMat, cv.COLOR_RGBA2GRAY);
+  let faces = [];
+  let eyes = [];
+  let size;
+  let faceVect = new cv.RectVector();
+  let faceMat = new cv.Mat();
+  cv.pyrDown(grayMat, faceMat);
+  cv.pyrDown(faceMat, faceMat);
+  size = faceMat.size();
+  faceClassifier.detectMultiScale(faceMat, faceVect);
+  for (let i = 0; i < faceVect.size(); i++) {
+    let face = faceVect.get(i);
+    faces.push(new cv.Rect(face.x, face.y, face.width, face.height));
+  }
+  faceMat.delete();
+  faceVect.delete();
+  canvasOutputCtx.drawImage(canvasInput, 0, 0, videoWidth, videoHeight);
+  drawResults(canvasOutputCtx, faces, 'red', size);
+  drawResults(canvasOutputCtx, eyes, 'yellow', size);
+  requestAnimationFrame(processVideo);
+}
+
+function drawResults(ctx, results, color, size) {
+  for (let i = 0; i < results.length; ++i) {
+    let rect = results[i];
+    let xRatio = videoWidth/size.width;
+    let yRatio = videoHeight/size.height;
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = color;
+    ctx.strokeRect(rect.x*xRatio, rect.y*yRatio, rect.width*xRatio, rect.height*yRatio);
+  }
+}
+function opencvIsReady() {
+  console.log('OpenCV.js is ready');
+  startCamera();
+}
+
+(function () {
+  //if (
+    //!"mediaDevices" in navigator ||
+    //!"getUserMedia" in navigator.mediaDevices
+  //) {
+    //alert("A API da câmera não está disponível no seu navegador");
+  //  return;
+  //}
+
+  // obter elementos da página
+  const video = document.querySelector("#video");
+  const btnPlay = document.querySelector("#btnPlay");
+  const btnPause = document.querySelector("#btnPause");
+  const btnScreenshot = document.querySelector("#btnScreenshot");
+  //const btnChangeCamera = document.querySelector("#btnChangeCamera");
+  const screenshotsContainer = document.querySelector("#screenshots");
+  const canvas = document.querySelector("#canvas");
+  const devicesSelect = document.querySelector("#devicesSelect");
+
+  // Restrições de vídeo
+  const constraints = {
+    video: {
+      width: {
+        min: 1280,
+        ideal: 1920,
+        max: 2560,
+      },
+      height: {
+        min: 720,
+        ideal: 1080,
+        max: 1440,
+      },
+    },
+  };
+
+  // Usar câmera frontal
+  let useFrontCamera = true;
+
+  // fluxo de vídeo atual
+  let videoStream;
+
+  // handle events
+  // play
+  btnPlay.addEventListener("click", function () {
+    video.play();
+    btnPlay.classList.add("is-hidden");
+    btnPause.classList.remove("is-hidden");
+  });
+
+  // pause
+  btnPause.addEventListener("click", function () {
+    video.pause();
+    btnPause.classList.add("is-hidden");
+    btnPlay.classList.remove("is-hidden");
+  });
+
+  // take screenshot
+  btnScreenshot.addEventListener("click", function () {
+    const img = document.createElement("img");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    canvas.getContext("2d").drawImage(video, 0, 0);
+    img.src = canvas.toDataURL("image/png");
+    screenshotsContainer.prepend(img);
+  });
+
+  // switch camera
+  //btnChangeCamera.addEventListener("click", function () {
+  //  useFrontCamera = !useFrontCamera;
+
+  //  initializeCamera();
+  //});
+
+  // stop video stream
+  function stopVideoStream() {
+    if (videoStream) {
+      videoStream.getTracks().forEach((track) => {
+        track.stop();
+      });
+    }
+  }
+
+  // initialize
+  async function initializeCamera() {
+    stopVideoStream();
+    constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+
+    try {
+      videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+      video.srcObject = videoStream;
+    } catch (err) {
+      alert("Could not access the camera");
+    }
+  }
+
+  initializeCamera();
+})();
+
+
